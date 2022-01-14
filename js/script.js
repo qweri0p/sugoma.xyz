548588b6abf7ed0e64430251a4279969d97009d2
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-01-14 17:05:39 +0100

    obama

--- js/script.js
+++ js/script.js
@@ -5,26 +5,19 @@ var array = [
     'content/yosus.png',
     'content/sus-monokuma.png',
     'content/sauce-game.png',
-    'content/mario-hill.png'
+    'content/mario-hill.png',
+    'content/obama.png'
 ];
 console.log(array)
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
-    }
-  
+    }  
     return array;
   }
 var shuffled_images = shuffle(array);
 console.log(shuffled_images)
-
