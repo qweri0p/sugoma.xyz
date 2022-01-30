04f6cfeacbd3483db7aa9150f905ddefdafb2f3e
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-01-30 16:17:24 +0100

    massive funny

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var array = [
-    'content/gex.jpeg', 
+    'content/gex.jpg',
     'content/ranno.png',
     'content/sumoga.jpg',
     'content/yosus.png',
     'content/sus-monokuma.png',
     'content/sauce-game.png',
     'content/mario-hill.png',
-    'content/obama.png'
+    'content/obama.png',
 ];
 console.log(array)
 function shuffle(array) {
